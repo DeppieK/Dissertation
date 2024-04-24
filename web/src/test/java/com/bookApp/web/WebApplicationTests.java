024344170c6e47a707baf2e4543be6c80b75c07f
DeppieK <deppie4@gmail.com>
2024-04-24 19:55:36 +0300

    spring boot app + postgresql

---
+++ web/src/test/java/com/bookApp/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookApp.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
