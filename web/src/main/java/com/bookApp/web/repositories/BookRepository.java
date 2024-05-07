ebb82e0a87a4bef285504fab81a1dd7b7c0a80d9
DeppieK <deppie4@gmail.com>
2024-05-07 12:06:20 +0300

    search query for future use

--- web/src/main/java/com/bookApp/web/repositories/BookRepository.java
+++ web/src/main/java/com/bookApp/web/repositories/BookRepository.java
@@ -1,4 +1,25 @@
 package com.bookApp.web.repositories;
 
-public interface BookRepository {
+import com.bookApp.web.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    /*
+    //the query for the search method
+        @Query("SELECT b FROM Book b WHERE " +
+                "LOWER(b.title) LIKE CONCAT('%', LOWER(:query), '%') OR " +
+                "LOWER(b.author) LIKE CONCAT('%', LOWER(:query), '%') OR " +
+                "LOWER(b.genre) LIKE CONCAT('%', LOWER(:query), '%') OR " +
+                "CAST(b.ISBN AS STRING) LIKE CONCAT('%', LOWER(:query), '%')")
+        List<Book> searchBooks(@Param("query") String query);
+    */
+
+    List<Book> findByISBN(Long isbn);
+    List<Book> findByGenre(String genre);
+    List<Book> findByAuthor(String author);
 }
