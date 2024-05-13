74899f0a48ffa7357b8f1056c7b0de5788495d06
DeppieK <deppie4@gmail.com>
2024-05-13 15:48:27 +0300

    details page

--- web/src/main/java/com/bookApp/web/controllers/BookController.java
+++ web/src/main/java/com/bookApp/web/controllers/BookController.java
@@ -1,5 +1,7 @@
 package com.bookApp.web.controllers;
 
+import com.bookApp.web.dto.BookDto;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.ui.Model;
 import com.bookApp.web.models.Book;
 import com.bookApp.web.repositories.BookRepository;
@@ -7,6 +9,7 @@ import com.bookApp.web.services.impl.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -26,9 +29,25 @@ public class BookController {
     //main page
     @GetMapping("/books")
     //@ModelAttribute
-    public String listBooks(Model model){
+    public String Books(Model model){
         List<Book> books = bookServiceImpl.getBook();
         model.addAttribute("books", books);
         return "index";
+    }
+
+    //details page
+    @GetMapping("/books/{bookId}")
+    //@ModelAttribute
+    public String bookDetail(@PathVariable("bookId") long bookId, Model model) throws ChangeSetPersister.NotFoundException {
+        BookDto bookDto = bookServiceImpl.findBookById(bookId);
+        //Long userId = commentsService.getUserIdByBookId(bookId); // Updated to Long
+
+        //List<Comments> comments = commentsService.getCommentsByBookId(bookId);
+
+        model.addAttribute("book", bookDto);
+        //model.addAttribute("comments", comments);
+        //model.addAttribute("user", userId);
+
+        return "detailsPage";
     }
 }
