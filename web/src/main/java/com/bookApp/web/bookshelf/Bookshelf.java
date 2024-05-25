b35edc395b589fb5b6c5d7522827fdd8805086bf
DeppieK <deppie4@gmail.com>
2024-05-25 12:38:28 +0300

    bookshelf model

---
+++ web/src/main/java/com/bookApp/web/bookshelf/Bookshelf.java
@@ -0,0 +1,70 @@
+package com.bookApp.web.bookshelf;
+
+import com.bookApp.web.book.Book;
+import com.bookApp.web.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@Setter
+@Getter
+@SpringBootApplication
+@ComponentScan("com.bookApp.web")
+@Entity
+@Table(name = "bookshelves")
+public class Bookshelf {
+    @Id
+    @SequenceGenerator(
+            name = "bookshelf_sequence",
+            sequenceName = "bookshelf_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "bookshelf_sequence"
+    )
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Column(name = "shelf_id", nullable = false)
+    private Long shelfId;
+
+    @Column(name = "label")
+    private String label;
+
+    // Constructors
+    public Bookshelf() {
+    }
+
+    public Bookshelf(Long id) {
+        this.id = id;
+    }
+
+    public Bookshelf(User user, Book book, Long shelfId, String label) {
+        this.user = user;
+        this.book = book;
+        this.shelfId = shelfId;
+        this.label = label;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Bookshelf{" +
+                "id=" + id +
+                ", user=" + user +
+                ", book=" + book +
+                ", shelfId=" + shelfId +
+                ", label='" + label + '\'' +
+                '}';
+    }
+}
