0eb40bcc94f4f34b935333a12c64a384ea56d343
DeppieK <deppie4@gmail.com>
2024-09-01 19:40:10 +0300

    changed app name

--- web/src/main/java/com/bookApp/web/bookshelf/BookshelfController.java
+++ web/src/main/java/com/bookApp/web/bookshelf/BookshelfController.java
@@ -4,10 +4,12 @@ import com.bookApp.web.book.BookRepository;
 import com.bookApp.web.book.BookService;
 import com.bookApp.web.user.User;
 import com.bookApp.web.user.UserService;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
@@ -40,6 +42,9 @@ public class BookshelfController {
     public String listBooks(Model model, Principal principal) {
         //get the current user
         User user = userService.findByUsername(principal.getName());
+
+        List<Bookshelf> bookshelves = bookshelfService.getBookshelfByUser(user);
+        model.addAttribute("bookshelves", bookshelves);
 
         //get the user's bookshelves with specified labels
         List<Bookshelf> specificLabelBookshelves = bookshelfService.getBookshelvesByUserAndLabels(user, List.of("currently_reading", "read", "want_to_read"));
@@ -79,8 +84,8 @@ public class BookshelfController {
         return "redirect:/myBooks"; //change this
     }
     //bookshelf details
-    @GetMapping("/myBookshelf")
-    public String myBookshelf(Model model, Principal principal) {
+    @GetMapping("/myBookshelf/{shelfId}")
+    public String myBookshelf(@PathVariable("shelfId") long shelfId, Model model, Principal principal) throws ChangeSetPersister.NotFoundException {
 
         // Get the current user
         User user = userService.findByUsername(principal.getName());
