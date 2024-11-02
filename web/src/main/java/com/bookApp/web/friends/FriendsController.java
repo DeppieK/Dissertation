2aff4521fc522f418b2cac1aba13b508e7241e3c
DeppieK <deppie4@gmail.com>
2024-11-02 18:11:58 +0200

    friends list (WIP)

---
+++ web/src/main/java/com/bookApp/web/friends/FriendsController.java
@@ -0,0 +1,32 @@
+package com.bookApp.web.friends;
+
+import com.bookApp.web.user.User;
+import com.bookApp.web.user.UserService;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class FriendsController {
+
+    @Autowired
+    private UserService userService;
+
+    public FriendsController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/myFriends")
+    public String friendsList(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        User user = userService.findByUsername(username);
+
+        return "friendsList";
+    }
+
+}
