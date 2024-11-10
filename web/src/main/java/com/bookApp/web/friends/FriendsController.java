8e1e308527182e2f93410760fb6e7a54c7025c0a
DeppieK <deppie4@gmail.com>
2024-11-10 22:46:32 +0200

    friends list buttons impl

--- web/src/main/java/com/bookApp/web/friends/FriendsController.java
+++ web/src/main/java/com/bookApp/web/friends/FriendsController.java
@@ -2,12 +2,16 @@ package com.bookApp.web.friends;
 
 import com.bookApp.web.user.User;
 import com.bookApp.web.user.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.security.Principal;
 import java.util.List;
 
 
@@ -43,4 +47,39 @@ public class FriendsController {
         return "friendsList";
     }
 
+    @GetMapping("/deleteRecord/{friendId}")
+    public ResponseEntity<String> deleteFriendRecord(@PathVariable Long friendId) {
+
+        Friends friend = friendsRepository.findById(friendId).orElse(null);
+
+        if (friend != null) {
+            friendsRepository.deleteById(friendId);
+            return ResponseEntity.ok().build();
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @GetMapping("/answerRequest/{friendId}/{answer}")
+    public ResponseEntity<String> answerFriendRequest(@PathVariable Long friendId, @PathVariable String answer) {
+
+        Friends friend = friendsRepository.findById(friendId).orElse(null);
+
+        if (friend != null) {
+            if (answer.equals("Yes")) {
+                friend.setStatus(Friends.Status.ACCEPTED);
+                friendsRepository.save(friend);
+                return ResponseEntity.ok().build();
+            }
+            else {
+                friend.setStatus(Friends.Status.DECLINED);
+                friendsRepository.save(friend);
+                return ResponseEntity.ok().build();
+            }
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 }
